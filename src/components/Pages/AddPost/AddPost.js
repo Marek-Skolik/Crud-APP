6809cef37314622153855db2199a426b11237afb
Marek Skolik <marek250296@interia.pl>
2022-12-01 11:34:15 +0100

    commit

--- src/components/Pages/AddPost/AddPost.js
+++ src/components/Pages/AddPost/AddPost.js
@@ -1,8 +1,20 @@
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import AddPostForm from '../../Features/AddPostForm/AddPostForm';
+import { addPost } from "../../../redux/postRedux";
 
 const AddPost = () => {
+
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+
+    const handleSubmit = post => {
+      dispatch(addPost(post));
+      navigate("/")
+    };
+
     return (
-        <AddPostForm />
+        <AddPostForm action={handleSubmit} />
     )
 }
 
